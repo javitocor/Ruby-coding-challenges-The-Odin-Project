46b2a6eb42e60c984e6046c9f3196ac094c79a3e
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-24 08:52:11 +0200

    solve stock picker

---
+++ stockPicker.rb
@@ -0,0 +1,22 @@
+def stock_picker(arr)
+  profit = []
+  value = 0
+  i = 0
+  while i < arr.length - 2     
+    j = 1
+    while j < arr.length - 1
+      num = arr[j] - arr[i]
+      if num > value 
+        profit[0] = i
+        profit[1] = j 
+        value = num       
+      end
+      j += 1
+    end
+    i += 1
+  end
+  return profit
+end
+
+p stock_picker([17,3,6,9,15,8,6,1,10])
+# [1,4]
