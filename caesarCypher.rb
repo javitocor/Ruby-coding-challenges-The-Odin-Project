474931deb4cf5a6771efce3298845e42663d828a
javitocor <56809963+javitocor@users.noreply.github.com>
2020-09-24 07:26:47 +0200

    solve caesar cipher

---
+++ caesarCypher.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, number)
+  array = string.bytes
+  array2 = array.map do |x| 
+    if x >= 65 && x <= 90
+      (((x - 65 + number) % 26) + 65).chr
+    elsif x >= 97 && x <= 122
+      (((x - 97 + number) % 26) + 97).chr
+    else 
+      x.chr
+    end
+  end
+  return array2.join('')
+end
+
+puts caesar_cipher("What a string!", 5)
+# "Bmfy f xywnsl!"
